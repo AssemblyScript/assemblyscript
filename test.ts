1fea4d529a49f81a6ac2a90da472135509539d97
Pannous <info@pannous.net>
2018-01-06 00:08:00 +0100

    ["short", Type.i16],
          ["int8", Type.i8],
          ["int16", Type.i16],
          ["int32", Type.i32],
          ["int64", Type.i64],

--- test.ts
+++ test.ts
@@ -3,7 +3,7 @@
 // var x=43 ERROR AS1110: Type expected. WHY NO INFERENCE?
 // var x:int=43
 
-export function main():int {
+export function main():int32 {
 	var x=42
 	var y=true or false
 	if(y)x++
