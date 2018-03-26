e1fa956928a2684aa93d6c0d8a88ae9270a1992c
Pannous <info@pannous.net>
2018-03-26 10:13:53 +0200

    prettier

--- examples/hello-string/hello-string.ts
+++ examples/hello-string/hello-string.ts
@@ -9,22 +9,25 @@
 // 		new WebAssembly.Instance(new WebAssembly.Module(binary),{console:{log_char}})"
 
 namespace console {
-
   // imported helper to print a string in node.js or browser
-  // export declare function logs(string_pointer: i32, length: i32): void; 
-  export declare function logs(string_pointer: i32, size: i32, encoding: i16): void; // size=2*length for utf16
+  // export declare function logs(string_pointer: i32, length: i32): void;
+  export declare function logs(
+    string_pointer: i32,
+    size: i32,
+    encoding: i16
+  ): void; // size=2*length for utf16
 
-  function log(text:string):void{
-  	logs(<i32>text+4,size(text),16);// 4 offset for internal struct // Utf16
+  function log(text: string): void {
+    logs(<i32>text + 4, size(text), 16); // 4 offset for internal struct // Utf16
   }
 }
 
-function size(pointer:string):i16{
-		return load<i16>(<i32>pointer)*2 // size=2*length for utf16
+function size(pointer: string): i16 {
+  return load<i16>(<i32>pointer) * 2; // size=2*length for utf16
 }
 
-export function main():void{
-	console.log("Hello AssemblyScript 😻	!")
+export function main(): void {
+  console.log("Hello AssemblyScript 😻	!");
 }
 
 // main()
