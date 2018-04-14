974459fb5a9d4cce9c74b06abbe91ff626310376
MaxGraey <maxgraey@gmail.com>
2018-04-14 22:26:38 +0300

    Improve popcnt

--- std/portable.js
+++ std/portable.js
@@ -81,13 +81,9 @@ globalScope["ctz"] = function ctz(value) {
 };
 
 globalScope["popcnt"] = function popcnt(value) {
-  value &= (1 << 31) - 1;
-  value -= value >>> 1 & 0x55555555;
-  value = (value & 0x33333333) + (value >>> 2 & 0x33333333);
-  value = value + (value >> 4) & 0x0F0F0F0F;
-  value += value >>> 8;
-  value += value >>> 16;
-  return value & 0x7F;
+  value -= ((value >>> 1) & 0x55555555);
+  value = (value & 0x33333333) + ((value >>> 2) & 0x33333333);
+  return (((value + (value >>> 4)) & 0x0F0F0F0F) * 0x01010101) >>> 24;
 };
 
 globalScope["rotl"] = function rotl(value, shift) {
