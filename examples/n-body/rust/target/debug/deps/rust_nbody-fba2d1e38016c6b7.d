be72cf2f2f5a9afd2942026eec12fb2ca68a060a
MaxGraey <maxgraey@gmail.com>
2018-04-29 20:35:40 +0300

    Remove unnecessary files

--- examples/n-body/rust/target/debug/deps/rust_nbody-fba2d1e38016c6b7.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Archive/Projects/Github/assemblyscript/examples/n-body/rust/target/debug/deps/rust_nbody-fba2d1e38016c6b7.rmeta: src/lib.rs
-
-/Volumes/Archive/Projects/Github/assemblyscript/examples/n-body/rust/target/debug/deps/rust_nbody-fba2d1e38016c6b7.d: src/lib.rs
-
-src/lib.rs:
