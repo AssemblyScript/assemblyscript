2e5a42d233ece1043b8fad48d8fb42545413654d
Max Graey <maxgraey@gmail.com>
2018-06-24 02:06:31 +0300

    Fix unexpected TS1005 in tryParseSignature (#149)

--- tests/parser/var.ts.fixture.ts
+++ tests/parser/var.ts.fixture.ts
@@ -4,5 +4,6 @@ const c: i32 = 0;
 var d = 2;
 var e;
 const f: i32;
+const t = 0 < (c / 10);
 // ERROR 1110: "Type expected." in var.ts:7:5
 // ERROR 1155: "'const' declarations must be initialized." in var.ts:10:6
