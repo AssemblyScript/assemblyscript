92ed9b8b392bb1fb4713dd644245f15cfa3a2342
MaxGraey <maxgraey@gmail.com>
2018-07-23 00:40:21 +0300

    avoid compaund compile check for perform right tree shaking

--- std/assembly/internal/array.ts
+++ std/assembly/internal/array.ts
@@ -10,14 +10,18 @@ import {
 /** Obtains the default comparator for the specified type. */
 @inline
 export function defaultComparator<T>(): (a: T, b: T) => i32 {
-  if (isInteger<T>() && isSigned<T>()) {
-    return (a: T, b: T): i32 => {
-      // JavaScript by default casting all values to string
-      // so for emulate this we use trick below
-      // TODO can we do better?
-      var t = a - b;
-      return select<T>(-t, t, a & b & (1 << 8 * sizeof<T>() - 1));
-    };
+  if (isInteger<T>()) {
+    if (isSigned<T>()) {
+      return (a: T, b: T): i32 => {
+        // JavaScript by default casting all values to string
+        // so for emulate this we use trick below
+        // TODO can we do better?
+        var t = a - b;
+        return select<T>(-t, t, a & b & (1 << 8 * sizeof<T>() - 1));
+      };
+    } else {
+      return (a: T, b: T): i32 => (<i32>(a > b) - <i32>(a < b));
+    }
   } else if (isFloat<T>()) {
     return (a: T, b: T): i32 => {
       // JavaScript by default casting all values to string
@@ -58,9 +62,6 @@ export function defaultComparatorTyped<T>(): (a: T, b: T) => i32 {
       };
     }
   } else {
-    if (isInteger<T>() && isSigned<T>() && sizeof<T>() <= 4) {
-      return (a: T, b: T): i32 => (<i32>(a - b));
-    }
     return (a: T, b: T): i32 => (<i32>(a > b) - <i32>(a < b));
   }
 }
