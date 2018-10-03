83a30df22e10c7dd0d5611ab196a4c99a27ad967
MaxGraey <maxgraey@gmail.com>
2018-10-03 15:00:06 +0300

    fix deprecation warning in Rust sources

--- examples/n-body/rust/src/lib.rs
+++ examples/n-body/rust/src/lib.rs
@@ -6,7 +6,7 @@
 use core::intrinsics;
 use core::panic::PanicInfo;
 
-#[panic_implementation]
+#[panic_handler]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
   unsafe { intrinsics::abort() }
