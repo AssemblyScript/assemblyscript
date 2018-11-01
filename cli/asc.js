820cd9734a2a717f1260f6557a2e47cfccb23486
dcodeIO <dcode@dcode.io>
2018-11-01 02:34:33 +0100

    Update to latest Binaryen

--- cli/asc.js
+++ cli/asc.js
@@ -22,8 +22,6 @@ const optionsUtil = require("./util/options");
 const mkdirp = require("./util/mkdirp");
 const EOL = process.platform === "win32" ? "\r\n" : "\n";
 
-global.Binaryen = require("../lib/binaryen");
-
 // Emscripten adds an `uncaughtException` listener to Binaryen that results in an additional
 // useless code fragment on top of an actual error. suppress this:
 if (process.removeAllListeners) process.removeAllListeners("uncaughtException");
