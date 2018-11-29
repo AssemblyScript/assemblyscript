ba56ef1c2ddce631aa94c3ddb2281f06c9240631
Nidin Vinayakan <01@01alchemist.com>
2018-11-29 14:59:49 +0100

    Wait wake example clean up

--- examples/atomic-wait-wake/assembly/index.ts
+++ examples/atomic-wait-wake/assembly/index.ts
@@ -1,4 +1,26 @@
+import "allocator/atomic";
+import { itoa } from "internal/number";
 
-export function wait(): void {
-  Atomic.wait<i64>(0, 0, -1);
+declare function log_str(v: string): void;
+
+var id = 1;
+type int = i32;
+// type int = i64;
+
+export function setId(_id: i32): void {
+  id = _id;
+}
+
+export function wait(mutexAddr: i32 = 0, value: int = 1): void {
+  log_str('[WASM][' + itoa<i32>(id) + '] waiting...');
+  Atomic.store<int>(mutexAddr, value);
+  Atomic.wait<int>(mutexAddr, value, -1);
+  log_str('[WASM][' + itoa<i32>(id) + '] waken');
+}
+
+export function wake(mutexAddr: i32 = 0, value: int = 0, numAgents: i32 = 1): void {
+  log_str('[WASM][' + itoa<i32>(id) + '] waking '+ itoa<i32>(numAgents) + ' agent(s)...');
+  Atomic.store<int>(mutexAddr, value);
+  var count = Atomic.wake<int>(mutexAddr, numAgents);
+  log_str('[WASM][' + itoa<i32>(id) + '] waken ' + itoa<i32>(count) + ' agent(s)');
 }
