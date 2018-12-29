846515ec527f3e6dd3917fe6842706553077f7af
Vladimir Grichina <vgrichina@gmail.com>
2018-12-28 23:01:26 -0800

    Add diff to ./test.sh based on snapshots of output

--- tests/near-bindgen/test.sh
+++ tests/near-bindgen/test.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
+set -e
+set -x
 BASEDIR=../../
-$BASEDIR/bin/asc main.ts --outFile main.wat --nearFile main.near.ts
-cp main.near.ts combined.ts
-prettier --write combined.ts
-$BASEDIR/bin/asc combined.ts -o combined.wat
-$BASEDIR/bin/asc test.ts -o test.wasm
-
+$BASEDIR/bin/asc main.ts --outFile main.wat --nearFile main_near.ts
+prettier --write main_near.ts
+$BASEDIR/bin/asc main_near.ts -o main_near.wat
+diff main_near.ts.expected main_near.ts
+$BASEDIR/bin/asc test.ts -o test.wasm --textFile test.wat
+diff test.wat.expected test.wat
