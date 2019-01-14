b8307f0afb82bd8a05c4791fb8d6f68e58d743f3
Willem Wyndham <willem.neal@gmail.com>
2019-01-14 16:25:17 -0500

    Fixed test to use asnyc.

--- tests/browser-asc.js
+++ tests/browser-asc.js
@@ -8,76 +8,78 @@ const stderr = asc.createMemoryStream();
 const files = { "module.ts": `import "allocator/arena";` };
 
 console.log("# asc --version");
+async function main() {
+  await asc.main([
+    "--version"
+  ], {
+    stdout: stdout,
+    stderr: stderr
+  }, err => {
+    console.log(">>> STDOUT >>>");
+    process.stdout.write(stdout.toString());
+    stdout.reset();
+    console.log(">>> STDERR >>>");
+    process.stdout.write(stderr.toString());
+    stderr.reset();
+  });
 
-asc.main([
-  "--version"
-], {
-  stdout: stdout,
-  stderr: stderr
-}, err => {
+  console.log("\n# asc --help");
+
+  await asc.main([
+    "--help"
+  ], {
+    stdout: stdout,
+    stderr: stderr
+  }, err => {
+    console.log(">>> STDOUT >>>");
+    process.stdout.write(stdout.toString());
+    stdout.reset();
+    console.log(">>> STDERR >>>");
+    process.stdout.write(stderr.toString());
+    stderr.reset();
+  });
+
+  console.log("\n# asc module.ts --textFile");
+
+  await asc.main([
+    "module.ts",
+    "--textFile"
+  ], {
+    stdout: stdout,
+    stderr: stderr,
+    readFile: (name, baseDir) => {
+      console.log("readFile: " + name + ", baseDir=" + baseDir);
+      if (files.hasOwnProperty(name)) return files[name];
+      return null;
+    },
+    writeFile: (name, data, baseDir) => {
+      console.log("writeFile: " + name + ", baseDir=" + baseDir);
+    },
+    listFiles: (dirname, baseDir) => {
+      console.log("listFiles: " + dirname + ", baseDir=" + baseDir);
+      return [];
+    }
+  }, err => {
+    if (err) {
+      console.log(">>> THROWN >>>");
+      console.log(err);
+    }
+  });
+
   console.log(">>> STDOUT >>>");
   process.stdout.write(stdout.toString());
-  stdout.reset();
   console.log(">>> STDERR >>>");
   process.stdout.write(stderr.toString());
-  stderr.reset();
-});
 
-console.log("\n# asc --help");
+  console.log("\n# asc.compileString");
 
-asc.main([
-  "--help"
-], {
-  stdout: stdout,
-  stderr: stderr
-}, err => {
-  console.log(">>> STDOUT >>>");
-  process.stdout.write(stdout.toString());
-  stdout.reset();
-  console.log(">>> STDERR >>>");
-  process.stdout.write(stderr.toString());
-  stderr.reset();
-});
-
-console.log("\n# asc module.ts --textFile");
-
-asc.main([
-  "module.ts",
-  "--textFile"
-], {
-  stdout: stdout,
-  stderr: stderr,
-  readFile: (name, baseDir) => {
-    console.log("readFile: " + name + ", baseDir=" + baseDir);
-    if (files.hasOwnProperty(name)) return files[name];
-    return null;
-  },
-  writeFile: (name, data, baseDir) => {
-    console.log("writeFile: " + name + ", baseDir=" + baseDir);
-  },
-  listFiles: (dirname, baseDir) => {
-    console.log("listFiles: " + dirname + ", baseDir=" + baseDir);
-    return [];
-  }
-}, err => {
-  if (err) {
-    console.log(">>> THROWN >>>");
-    console.log(err);
-  }
-});
-
-console.log(">>> STDOUT >>>");
-process.stdout.write(stdout.toString());
-console.log(">>> STDERR >>>");
-process.stdout.write(stderr.toString());
-
-console.log("\n# asc.compileString");
-
-const output = asc.compileString(`import "allocator/arena";`, { optimizeLevel: 2 });
-console.log(">>> .stdout >>>");
-process.stdout.write(output.stdout.toString());
-console.log(">>> .stderr >>>");
-process.stdout.write(output.stderr.toString());
-console.log(">>> .text >>>");
-process.stdout.write(output.text);
-console.log(">>> .binary >>> " + output.binary.length + " bytes");
+  const output = await asc.compileString(`import "allocator/arena";`, { optimizeLevel: 2 });
+  console.log(">>> .stdout >>>");
+  process.stdout.write(output.stdout.toString());
+  console.log(">>> .stderr >>>");
+  process.stdout.write(output.stderr.toString());
+  console.log(">>> .text >>>");
+  process.stdout.write(output.text);
+  console.log(">>> .binary >>> " + output.binary.length + " bytes");
+}
+main();
