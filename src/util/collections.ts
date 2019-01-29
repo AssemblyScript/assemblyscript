317434da4113fa6a3767ebef569fbcd6aedc7ad7
MaxGraey <maxgraey@gmail.com>
2019-01-29 02:21:26 +0200

    substring -> substr

--- src/util/collections.ts
+++ src/util/collections.ts
@@ -1,7 +1,8 @@
 export function makeArray<V>(original: Array<V> | null = null): Array<V> {
   if (original) {
-    let cloned = new Array<V>(original.length);
-    for (let i = 0, k = original.length; i < k; ++i) unchecked(cloned[i] = original[i]);
+    let k = original.length;
+    let cloned = new Array<V>(k);
+    for (let i = 0; i < k; ++i) unchecked(cloned[i] = original[i]);
     return cloned;
   }
   return new Array<V>();
