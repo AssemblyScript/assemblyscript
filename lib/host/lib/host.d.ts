f4b3ab1a0788e8307160b051c356686d20a53f68
Willem Wyndham <willem.neal@gmail.com>
2019-02-19 11:26:29 -0500

    Forgot js and types

--- lib/host/lib/host.d.ts
+++ lib/host/lib/host.d.ts
@@ -1,7 +1,6 @@
 import { ASImport } from "./ASImport";
 export declare class Host extends ASImport {
-    stdout: (out: string) => void;
-    constructor(stdout?: (out: string) => void);
+    static stdout: (out: string) => void;
     debug(): void;
     _log(start: number, sizeof: number): void;
     _log_str(x: number): void;
