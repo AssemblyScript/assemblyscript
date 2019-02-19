50185296ac6354f8c7a5da77a324b94808d32a6c
Willem Wyndham <willem.neal@gmail.com>
2019-02-19 09:54:48 -0500

    update compiled js and types

--- lib/host/lib/host.d.ts
+++ lib/host/lib/host.d.ts
@@ -1,5 +1,7 @@
 import { ASImport } from "./ASImport";
 export declare class Host extends ASImport {
+    stdout: (out: string) => void;
+    constructor(stdout?: (out: string) => void);
     debug(): void;
     _log(start: number, sizeof: number): void;
     _log_str(x: number): void;
