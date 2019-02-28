55d07b6cb0460fae63c0cfb6730dba462ba2c560
Nidin Vinayakan <01@01alchemist.com>
2019-02-28 15:21:57 +0100

    Atomic memory allocator
    Atomic allocator for shared memory

---
+++ std/assembly/allocator/atomic.ts
@@ -0,0 +1,49 @@
+import { AL_MASK, MAX_SIZE_32 } from "../internal/allocator";
+
+var SATRT_OFFSET: usize = (HEAP_BASE + AL_MASK) & ~AL_MASK;
+var OFFSET_PTR: usize = SATRT_OFFSET;
+var TOP = (HEAP_BASE + 8 + AL_MASK) & ~AL_MASK;
+store<usize>(OFFSET_PTR, TOP);
+
+@global export function __allocator_get_offset(): usize {
+  return Atomic.load<usize>(OFFSET_PTR);
+}
+
+@global export function __allocator_set_offset(oldOffset: usize, newOffset: usize): usize {
+  return Atomic.cmpxchg<usize>(OFFSET_PTR, oldOffset, newOffset);
+}
+
+@global export function __memory_allocate(size: usize): usize {
+  if (size) {
+    if (size > MAX_SIZE_32) unreachable();
+    let currentOffset: usize;
+    let top: usize;
+    do {
+      currentOffset = __allocator_get_offset();
+      top = (currentOffset + size + AL_MASK) & ~AL_MASK;
+      let pagesBefore = memory.size();
+      if (top > (<usize>pagesBefore) << 16) {
+        let pagesNeeded = ((top - currentOffset + 0xffff) & ~0xffff) >>> 16;
+        let pagesWanted = max(pagesBefore, pagesNeeded); // double memory
+        if (memory.grow(pagesWanted) < 0) {
+          if (memory.grow(pagesNeeded) < 0) {
+            unreachable(); // out of memory
+          }
+        }
+      }
+    } while (
+      Atomic.cmpxchg<usize>(OFFSET_PTR, currentOffset, top) != currentOffset
+    );
+
+    return currentOffset;
+  }
+  return 0;
+}
+
+@global export function __memory_free(ptr: usize): void {
+ //TODO: Not implemented
+}
+
+@global export function __memory_reset(): void {
+  Atomic.store<usize>(OFFSET_PTR, SATRT_OFFSET);
+}
