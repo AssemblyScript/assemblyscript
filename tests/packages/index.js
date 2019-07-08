9aa137bbd29442ce31e5be853d8316c76da1ef66
Willem Wyndham <willem.neal@gmail.com>
2019-07-08 13:43:26 -0400

    Removed using npm all together

--- tests/packages/index.js
+++ tests/packages/index.js
@@ -1,10 +1,23 @@
+let glob = require("glob").sync
 let cp = require('child_process')
-let cmd = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
+let path = require('path');
 
-cp.spawn(cmd, ['run', 'test'], { env: process.env, cwd: __dirname, stdio: 'inherit' })
-      .on("error", (error) => {console.error(error); process.exit(1)})
-      .on("close", (code, signal) => {
-      if (code != 0 ){
-          process.exit(1);
-      }});
+function exec(commands) {
+    glob(__dirname+"/**/packages/*").forEach((_path) => {
+        let buildcommands = require(path.join(_path, 'package.json')).scripts.build.split(" && ")
+        for (let cmds of buildcommands){
+            let child = cp.spawn('node', cmds.split(" "), { env: process.env, cwd: _path, stdio: 'inherit' })
+                .on("error", (error) => {console.error(error); process.exit(1)})
+                .on("close", (code, signal) => {
+                if (code != 0 ){
+                    console.error(path.basename(_path) + " Failed");
+                    process.exit(1);
+                }else {
+                    console.log(path.basename(_path) + " Passed");
+                }});
 
+        }
+    });
+}
+
+exec(process.argv.slice(2))
--- tests/packages/packages/g/test.js
+++ tests/packages/packages/g/test.js
@@ -1,29 +1,39 @@
-let glob = require("glob").sync
-let cp = require('child_process')
-let cmd = /^win/.test(process.platform) ? 'npm.cmd' : 'npm';
-
+#!/usr/bin/env node
+let glob = require("glob").sync;
+let cp = require("child_process");
+let cmd = /^win/.test(process.platform) ? "npm.cmd" : "npm";
+let path = require("path");
 let buffer = [];
 
-function exec(commands) {
-    let child;
-    child = cp.spawn(cmd, commands, { env: process.env, cwd: __dirname })
-        .on("error", (error) => {
-            console.error("ERROR "+ error ); 
-            process.exit(1);
-            
-        })
-        .on("close", (code, signal) => {            
-            let res = buffer.join("");
-            if (/ERROR.*Import file .*lib\/a.ts.* not found/g.test(res)) {
-                process.exit(0);
-            } else {
-                process.exit(1);
-            }
-        });
-    child.stderr.on('data', (chunk) => {
-        buffer.push(chunk.toString());
-        });
-
+function exec() {
+    let commands = [
+      path.join(__dirname, "..", "..", "node_modules", ".bin", "asc"),
+      "assembly/index.ts",
+      "--noEmit",
+      "--runtime",
+      "stub",
+      "--validate",
+      "--traceResolution"
+    ];
+  let child;
+  child = cp
+    .spawn("node", commands, { env: process.env, cwd: __dirname })
+    .on("error", error => {
+      console.error("ERROR " + error);
+      process.exit(1);
+    })
+    .on("close", (code, signal) => {
+      let res = buffer.join("");
+      if (/ERROR.*Import file .*lib\/a.ts.* not found/g.test(res)) {
+        process.exit(0);
+      } else {
+        process.exit(1);
+      }
+    });
+  child.stderr.on("data", chunk => {
+    buffer.push(chunk.toString());
+  });
 }
+exec();
 
-exec(process.argv.slice(2))
+
