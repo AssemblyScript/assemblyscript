3f758c0555d54f1cd4cb8d04415982ad6c030aa5
MaxGraey <maxgraey@gmail.com>
2019-08-21 17:14:52 +0300

    remove parentesis

--- std/assembly/math.ts
+++ std/assembly/math.ts
@@ -62,7 +62,7 @@ function dtoi32(x: f64): i32 {
     let result = 0;
     let u = reinterpret<u64>(x);
     let e = (u >> 52) & 0x7ff;
-    if (e <= (1023 + 30)) {
+    if (e <= 1023 + 30) {
       result = <i32>x;
     } else if (e <= 1023 + 30 + 53) {
       let v = (u & ((<u64>1 << 52) - 1)) | (<u64>1 << 52);
