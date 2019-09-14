7ce570961a7739cb25433c93ed7c517aa19f3354
Willem Wyndham <willem.neal@gmail.com>
2019-09-14 17:02:22 -0400

    temp

---
+++ tests/compiler/interface.ts
@@ -0,0 +1,18 @@
+
+interface IFoo {
+  foo(i: i32): i32;
+}
+
+class AFoo implements IFoo {
+  i: i32 = 41;
+
+  foo(i: i32): i32 {
+    return this.i + i;
+  }
+}
+
+let aFoo = new AFoo();
+
+function passAnInterface(foo: IFoo): void {
+  assert(foo.foo(1) == 42);
+}
