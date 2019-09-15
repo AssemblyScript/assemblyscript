be5c431c946854829aed6570f7d42670e2d5e3a4
dcode <dcode@dcode.io>
2019-09-15 23:03:51 +0200

    add test

---
+++ tests/compiler/resolve-elementaccess.ts
@@ -0,0 +1,18 @@
+var arr = new Float32Array(2);
+arr[0] = 1;
+arr[1] = 2;
+assert(
+  arr[0].toString()
+  ==
+  "1.0"
+);
+assert(
+  (arr[1]).toString()
+  ==
+  "2.0"
+);
+assert(
+  (arr[0] += 10).toString()
+  ==
+  "11.0"
+);
