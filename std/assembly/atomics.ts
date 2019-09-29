0e153b583e62ad19c3d69f56aeb5882fd9565e9d
MaxGraey <maxgraey@gmail.com>
2019-09-29 03:59:04 +0300

    Simplify checks

--- std/assembly/atomics.ts
+++ std/assembly/atomics.ts
@@ -4,7 +4,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function load<T>(array: ArrayBufferView, index: i32): T {
-    if (isFloat<T>()) ERROR("Atomics.load accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.load<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset);
   }
@@ -12,7 +11,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function store<T>(array: ArrayBufferView, index: i32, value: T): void {
-    if (isFloat<T>()) ERROR("Atomics.store accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     atomic.store<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, value, index);
   }
@@ -20,7 +18,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function add<T>(array: ArrayBufferView, index: i32, value: T): T {
-    if (isFloat<T>()) ERROR("Atomics.add accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.add<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, value, index);
   }
@@ -28,7 +25,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function sub<T>(array: ArrayBufferView, index: i32, value: T): T {
-    if (isFloat<T>()) ERROR("Atomics.sub accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.sub<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, value, index);
   }
@@ -36,7 +32,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function and<T>(array: ArrayBufferView, index: i32, value: T): T {
-    if (isFloat<T>()) ERROR("Atomics.and accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.and<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, value, index);
   }
@@ -44,7 +39,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function or<T>(array: ArrayBufferView, index: i32, value: T): T {
-    if (isFloat<T>()) ERROR("Atomics.or accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.or<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, value, index);
   }
@@ -52,7 +46,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function xor<T>(array: ArrayBufferView, index: i32, value: T): T {
-    if (isFloat<T>()) ERROR("Atomics.xor accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.xor<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, value, index);
   }
@@ -60,7 +53,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function exchange<T>(array: ArrayBufferView, index: i32, value: T): T {
-    if (isFloat<T>()) ERROR("Atomics.exchange accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.xchg<T>(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, value, index);
   }
@@ -68,7 +60,6 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function compareExchange<T>(array: ArrayBufferView, index: i32, expectedValue: T, replacementValue: T): T {
-    if (isFloat<T>()) ERROR("Atomics.compareExchange accept only integer shared typed arrays");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.cmpxchg<T>(
       changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset,
@@ -81,16 +72,12 @@ export namespace Atomics {
   // @ts-ignore: decorator
   @inline
   export function wait<T>(array: ArrayBufferView, value: T, timeout: i64 = -1): AtomicWaitResult {
-    if (sizeof<T>() < 4) ERROR("Atomics.wait accept only Int32Array/Uint32Array or Int64Array/Uint64Array shared typed array");
-    if (isFloat<T>()) ERROR("Atomics.wait accept only Int32Array/Uint32Array or Int64Array/Uint64Array shared typed array");
     return atomic.wait<T>(changetype<usize>(array.data) + array.byteOffset, value, timeout);
   }
 
   // @ts-ignore: decorator
   @inline
   export function notify<T>(array: ArrayBufferView, index: i32, count: i32 = -1): i32 {
-    if (sizeof<T>() < 4) ERROR("Atomics.notify accept only Int32Array/Uint32Array or Int64Array/Uint64Array shared typed array");
-    if (isFloat<T>()) ERROR("Atomics.notify accept only Int32Array/Uint32Array or Int64Array/Uint64Array shared typed array");
     if (index < 0 || (index << alignof<T>()) >= array.byteLength) throw new RangeError("Invalid atomic access index");
     return atomic.notify(changetype<usize>(array.data) + (index << alignof<T>()) + array.byteOffset, count);
   }
