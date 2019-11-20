5b28805dda97823f3f3a471e41258c8062bebd70
MaxGraey <maxgraey@gmail.com>
2019-11-20 03:52:58 +0200

    use lookup tables for ascii & shrink level < 1

--- std/assembly/util/string.ts
+++ std/assembly/util/string.ts
@@ -1,6 +1,41 @@
 import { itoa, dtoa, itoa_stream, dtoa_stream, MAX_DOUBLE_LENGTH } from "./number";
 import { ipow32 } from "../math";
 
+// @ts-ignore
+@lazy const lowerTable127: u8[] = [
+  0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+  16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,
+  32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
+  48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,
+  64,
+  97,98,99,100,101,102,103,104,105,106,107,108,109,
+  110,111,112,113,114,115,116,117,118,119,120,121,122,
+  91,92,93,94,95,96,
+  97,98,99,100,101,102,103,104,105,106,107,108,109,
+  110,111,112,113,114,115,116,117,118,119,120,121,122,
+  123,124,125,126,127
+];
+
+// @ts-ignore
+@lazy const upperTable127: u8[] = [
+  0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+  16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,
+  32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,
+  48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,
+  64,
+  65,66,67,68,69,70,71,72,73,74,75,76,77,
+  78,79,80,81,82,83,84,85,86,87,88,89,90,
+  91,92,93,94,95,96,
+  65,66,67,68,69,70,71,72,73,74,75,76,77,
+  78,79,80,81,82,83,84,85,86,87,88,89,90,
+  123,124,125,126,127
+];
+
+// @ts-ignore
+@lazy const lowerTable127Ptr = lowerTable127.dataStart as usize;
+// @ts-ignore
+@lazy const upperTable127Ptr = upperTable127.dataStart as usize;
+
 // @ts-ignore: decorator
 @inline
 export const enum CharCode {
@@ -596,12 +631,20 @@ export function isUpper8(c: u32): bool {
 
 // @ts-ignore: decorator
 @inline
-export function toLower8(c: u32): u32 {
-  return select<u32>(c | 32, c, isUpper8(c));
+export function toLower8(c: i32): u32 {
+  if (ASC_SHRINK_LEVEL > 0) {
+    return select<u32>(c | 32, c, isUpper8(c));
+  } else {
+    return <u32>load<u8>(lowerTable127Ptr + c);
+  }
 }
 
 // @ts-ignore: decorator
 @inline
-export function toUpper8(c: u32): u32 {
-  return select<u32>(c & 0x5F, c, isLower8(c));
+export function toUpper8(c: i32): u32 {
+  if (ASC_SHRINK_LEVEL > 0) {
+    return select<u32>(c & 0x5F, c, isLower8(c));
+  } else {
+    return <u32>load<u8>(upperTable127Ptr + c);
+  }
 }
