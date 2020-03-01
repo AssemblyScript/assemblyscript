0ac67d244b53e6b3d257ce50d5304c5ec8bb333b
MaxGraey <maxgraey@gmail.com>
2020-03-01 11:56:27 +0200

    add comment

--- cli/asc.js
+++ cli/asc.js
@@ -667,7 +667,7 @@ exports.main = function main(argv, options, callback) {
         add("ssa-nomerge");
       }
       if (optimizeLevel >= 3) {
-        add("simplify-locals-nostructure");
+        add("simplify-locals-nostructure"); // differs
         add("flatten");
         add("local-cse");
       }
