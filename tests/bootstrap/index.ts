36e7470b24c900f6438ebd5fedaf5aa30f261c6f
MaxGraey <maxgraey@gmail.com>
2020-03-14 03:57:13 +0200

    post-merge fixes

--- tests/bootstrap/index.ts
+++ tests/bootstrap/index.ts
@@ -10,13 +10,13 @@ async function test(build: string): Promise<void> {
     fs.promises.readFile(`${ __dirname }/../../out/assemblyscript.${ build }.wasm`),
     { binaryen }
   );
+  console.log(util.inspect(asc, true));
   const optionsPtr = asc.newOptions();
   const programPtr = asc.newProgram(optionsPtr);
   const textPtr = asc.__allocString("export function add(a: i32, b: i32): i32 { return a + b; }\n");
   const pathPtr = asc.__allocString("index.ts");
   asc.parse(programPtr, textPtr, pathPtr, true);
   var nextFilePtr = asc.nextFile(programPtr);
-  console.log(util.inspect(assemblyscript, true));
   while (nextFilePtr) {
     console.log("nextFile: " + asc.__getString(nextFilePtr));
     nextFilePtr = asc.nextFile(programPtr);
