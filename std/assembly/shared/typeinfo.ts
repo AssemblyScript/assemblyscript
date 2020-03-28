9e059061cc082766fb69ebf63cd6445a8c35be53
jtenner <tenner.joshua@gmail.com>
2020-03-28 02:05:55 -0400

    Support StaticArray in loader (#1197)

--- std/assembly/shared/typeinfo.ts
+++ std/assembly/shared/typeinfo.ts
@@ -29,46 +29,48 @@ export const enum TypeinfoFlags {
   ARRAYBUFFERVIEW = 1 << 0,
   /** Type is an `Array`. */
   ARRAY = 1 << 1,
+  /** Type is a `StaticArray`. */
+  STATICARRAY = 1 << 2,
   /** Type is a `Set`. */
-  SET = 1 << 2,
+  SET = 1 << 3,
   /** Type is a `Map`. */
-  MAP = 1 << 3,
+  MAP = 1 << 4,
   /** Type is inherently acyclic. */
-  ACYCLIC = 1 << 4,
+  ACYCLIC = 1 << 5,
   /** Value alignment of 1 byte. */
-  VALUE_ALIGN_0 = 1 << 5,
+  VALUE_ALIGN_0 = 1 << 6,
   /** Value alignment of 2 bytes. */
-  VALUE_ALIGN_1 = 1 << 6,
+  VALUE_ALIGN_1 = 1 << 7,
   /** Value alignment of 4 bytes. */
-  VALUE_ALIGN_2 = 1 << 7,
+  VALUE_ALIGN_2 = 1 << 8,
   /** Value alignment of 8 bytes. */
-  VALUE_ALIGN_3 = 1 << 8,
+  VALUE_ALIGN_3 = 1 << 9,
   /** Value alignment of 16 bytes. */
-  VALUE_ALIGN_4 = 1 << 9,
+  VALUE_ALIGN_4 = 1 << 10,
   /** Value is a signed type. */
-  VALUE_SIGNED = 1 << 10,
+  VALUE_SIGNED = 1 << 11,
   /** Value is a float type. */
-  VALUE_FLOAT = 1 << 11,
+  VALUE_FLOAT = 1 << 12,
   /** Value type is nullable. */
-  VALUE_NULLABLE = 1 << 12,
+  VALUE_NULLABLE = 1 << 13,
   /** Value type is managed. */
-  VALUE_MANAGED = 1 << 13,
+  VALUE_MANAGED = 1 << 14,
   /** Key alignment of 1 byte. */
-  KEY_ALIGN_0 = 1 << 14,
+  KEY_ALIGN_0 = 1 << 15,
   /** Key alignment of 2 bytes. */
-  KEY_ALIGN_1 = 1 << 15,
+  KEY_ALIGN_1 = 1 << 16,
   /** Key alignment of 4 bytes. */
-  KEY_ALIGN_2 = 1 << 16,
+  KEY_ALIGN_2 = 1 << 17,
   /** Key alignment of 8 bytes. */
-  KEY_ALIGN_3 = 1 << 17,
+  KEY_ALIGN_3 = 1 << 18,
   /** Key alignment of 16 bytes. */
-  KEY_ALIGN_4 = 1 << 18,
+  KEY_ALIGN_4 = 1 << 19,
   /** Key is a signed type. */
-  KEY_SIGNED = 1 << 19,
+  KEY_SIGNED = 1 << 20,
   /** Key is a float type. */
-  KEY_FLOAT = 1 << 20,
+  KEY_FLOAT = 1 << 21,
   /** Key type is nullable. */
-  KEY_NULLABLE = 1 << 21,
+  KEY_NULLABLE = 1 << 22,
   /** Key type is managed. */
-  KEY_MANAGED = 1 << 22
+  KEY_MANAGED = 1 << 23
 }
