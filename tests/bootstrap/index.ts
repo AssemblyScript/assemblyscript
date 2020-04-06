6fcd1570368a70d9698c1948a52a4705194a4e2f
dcode <dcode@dcode.io>
2020-04-06 16:32:04 +0200

    extend bootstrap

--- tests/bootstrap/index.ts
+++ tests/bootstrap/index.ts
@@ -1,28 +1,82 @@
 import * as fs from "fs";
+import * as path from "path";
 import * as binaryen from "binaryen";
 import * as util from "util";
 import * as loader from "../../lib/loader";
+import * as find from "../../cli/util/find";
 import AssemblyScript from "../../out/assemblyscript";
+
+// Load stdlib
+const libDir = path.join(__dirname, "..", "..", "std", "assembly");
+const libraryFiles = {};
+find.files(libDir, find.TS_EXCEPT_DTS).forEach((file: string) => {
+  libraryFiles[file.replace(/\.ts$/, "")] = fs.readFileSync(path.join(libDir, file), "utf8" );
+});
 
 async function test(build: string): Promise<void> {
   await binaryen.ready;
+
   const { exports: asc } = await loader.instantiate<typeof AssemblyScript>(
     fs.promises.readFile(`${ __dirname }/../../out/assemblyscript.${ build }.wasm`),
     { binaryen }
   );
   console.log(util.inspect(asc, true));
+
+  const cachedStrings = new Map<string,number>();
+  function allocString(text: string): number {
+    if (cachedStrings.has(text)) return cachedStrings.get(text);
+    var ptr = asc.__retain(asc.__allocString(text));
+    cachedStrings.set(text, ptr);
+    return ptr;
+  }
+
   const optionsPtr = asc.newOptions();
   const programPtr = asc.newProgram(optionsPtr);
-  const textPtr = asc.__allocString("export function add(a: i32, b: i32): i32 { return a + b; }\n");
-  const pathPtr = asc.__allocString("index.ts");
-  asc.parse(programPtr, textPtr, pathPtr, true);
+
+  console.log("\nParsing standard library ...");
+  Object.keys(libraryFiles).forEach((libPath: string) => {
+    if (libPath.indexOf("/") >= 0) return;
+    const textPtr = allocString(libraryFiles[libPath]);
+    const pathPtr = allocString("~lib/" + libPath + ".ts");
+    console.log("  " + asc.__getString(pathPtr));
+    asc.parse(programPtr, textPtr, pathPtr, false);
+  });
+
+  console.log("\nParsing backlog ...");
   var nextFilePtr = asc.nextFile(programPtr);
   while (nextFilePtr) {
-    console.log("nextFile: " + asc.__getString(nextFilePtr));
+    const nextFile = asc.__getString(nextFilePtr);
+    if (!nextFile.startsWith("~lib/")) throw Error("unexpected file: " + nextFile);
+    const text = libraryFiles[nextFile.substring(5)];
+    if (text == null) throw Error("missing file: " + nextFile);
+    const textPtr = allocString(libraryFiles[nextFile.substring(5)]);
+    const pathPtr = allocString(nextFile + ".ts");
+    console.log("  " + asc.__getString(pathPtr));
+    asc.parse(programPtr, textPtr, pathPtr, false);
+    asc.__release(nextFilePtr);
     nextFilePtr = asc.nextFile(programPtr);
   }
-  // asc.compile(programPtr);
-  // ^ abort: missing ArrayBuffer at src/program.ts:1015:18
-  console.log("So far, so good.");
+
+  console.log("\nParsing entry file ...");
+  {
+    const textPtr = allocString("export function add(a: i32, b: i32): i32 { return a + b; }\n");
+    const pathPtr = allocString("index.ts");
+    console.log("  " + asc.__getString(pathPtr));
+    asc.parse(programPtr, textPtr, pathPtr, true);
+  }
+
+  try {
+    console.log("\nInitializing program ...");
+    asc.initializeProgram(programPtr, optionsPtr);
+    console.log("\nCompiling program ...");
+    const modulePtr = asc.compile(programPtr);
+    console.log(binaryen.wrapModule(modulePtr).emitText());
+  } catch (e) {
+    console.log("\nTODO: " + e.message);
+  }
+
+  cachedStrings.forEach(asc.__release);
+
+  console.log("\nSo far, so good.");
 }
 test("untouched");
