2d334a4c84c937a37b04efdb789427a045f1a1b8
jtenner <tenner.joshua@gmail.com>
2020-05-14 17:08:20 -0400

    Remove printrtti command line option (#1270)

--- cli/asc.d.ts
+++ cli/asc.d.ts
@@ -139,8 +139,6 @@ interface CompilerOptions {
   listFiles?: boolean;
   /** Prints measuring information on I/O and compile times. */
   measure?: boolean;
-  /** Prints the module's runtime type information to stderr. */
-  printrtti?: boolean;
   /** Disables terminal colors. */
   noColors?: boolean;
   /** Specifies an alternative file extension. */
