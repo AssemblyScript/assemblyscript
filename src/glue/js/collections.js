d29c9438112f3e592d7de64d14f81afce36a6d0a
MaxGraey <maxgraey@gmail.com>
2020-06-15 17:46:05 +0300

    Faster?

--- src/glue/js/collections.js
+++ src/glue/js/collections.js
@@ -4,13 +4,28 @@
  */
 
 global.Map_keys = function Map_keys(map) {
-  return Array.from(map.keys());
+  const arr = new Array(map.size);
+  let i = 0;
+  for (let key of map.keys()) {
+    arr[i++] = key;
+  }
+  return arr;
 };
 
 global.Map_values = function Map_values(map) {
-  return Array.from(map.values());
+  const arr = new Array(map.size);
+  let i = 0;
+  for (let value of map.values()) {
+    arr[i++] = value;
+  }
+  return arr;
 };
 
 global.Set_values = function Set_values(set) {
-  return Array.from(set.values());
+  const arr = new Array(set.size);
+  let i = 0;
+  for (let value of set.values()) {
+    arr[i++] = value;
+  }
+  return arr;
 };
