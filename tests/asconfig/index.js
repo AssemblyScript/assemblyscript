ca954306d9e7068831271cbaf66fc618a072cf27
Joshua Tenner <tenner.joshua@gmail.com>
2020-07-01 16:13:05 -0400

    add const test

---
+++ tests/asconfig/index.js
@@ -0,0 +1,26 @@
+const asc = require("../../cli/asc");
+const loader = require("../../lib/loader");
+/** @type {Uint8Array} */
+let binary;
+asc.main(["assembly/index.ts", "--outFile", "output.wasm", "--explicitStart"], {
+  writeFile(name, contents) {
+    if (name === "output.wasm") {
+      binary = contents;
+    }
+  }
+}, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  if (!binary) {
+    console.error("No binary was generated for the asconfig test in " + process.cwd());
+    process.exit(1);
+  }
+
+  const theResult = loader.instantiateSync(binary);
+
+  theResult.exports._start();
+  return 0;
+});
