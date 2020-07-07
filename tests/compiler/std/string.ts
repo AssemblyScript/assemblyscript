65446f9087ae30504e3973df7d4885fbf5effa7a
MaxGraey <maxgraey@gmail.com>
2020-07-07 08:57:56 +0300

    more tests

--- tests/compiler/std/string.ts
+++ tests/compiler/std/string.ts
@@ -108,9 +108,13 @@ assert(parseInt("0x1g") == 1); // not valid
 assert(parseInt("-123") == -123);
 assert(parseInt("+123") == 123);
 assert(parseInt("-12.3") == -12);
+assert(parseInt("0", 0) == 0);
+assert(parseInt("0x0", 0) == 0);
 assert(parseInt("-0") == -0 && Math.signbit(parseInt("-0")));
+assert(parseInt("0XABCDEF") == parseInt("ABCDEF", 16));
 assert(parseInt("36893488147419103232") == 36893488147419103000.0); // bigger than 2 ** 64 - 1
 assert(parseInt("36893488147419103232", 13) == 5.135857308667095e+21);
+assert(parseInt("-1000000000000000000000000", 10) == -1000000000000000000000000.0);
 assert(parseInt("1🔥") == 1);
 assert(parseInt("2badnum") == 2);
 
