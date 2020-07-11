f44491c4dabead9bf965519d786744e81f6818a2
Joshua Tenner <tenner.joshua@gmail.com>
2020-07-11 14:04:13 -0400

    cleanup

--- tests/asconfig/index.js
+++ tests/asconfig/index.js
@@ -22,10 +22,10 @@ asc.main(["assembly/index.ts", "--outFile", "output.wasm", "--explicitStart", ..
     console.error("No binary was generated for the asconfig test in " + process.cwd());
     process.exit(1);
   }
-  const optionsPath = path.join(process.cwd(), "options.json")
+  const optionsPath = path.join(process.cwd(), "options.json");
   if (fs.existsSync(optionsPath)) {
     const options = require(optionsPath);
-    for (let option of Object.getOwnPropertyNames(options) ){
+    for (const option of Object.getOwnPropertyNames(options) ){
       if (options[option] != asc.args[option]) {
         throw new Error(`Test ${path.basename(process.cwd())}: ${options[option]} != ${asc.args[option]}`);
       }
