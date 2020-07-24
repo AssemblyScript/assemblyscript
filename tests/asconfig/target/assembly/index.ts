399ae2359658d110c9e1aecda4fac7a0c4b27b54
Willem Wyndham <willem@cs.umd.edu>
2020-07-24 13:56:31 -0400

    Add more tests for asconfig

--- tests/asconfig/target/assembly/index.ts
+++ tests/asconfig/target/assembly/index.ts
@@ -1,3 +1,3 @@
-assert(ASC_OPTIMIZE_LEVEL == 3);
-assert(ASC_SHRINK_LEVEL == 1);
-assert(ASC_FEATURE_SIMD);
+assert(ASC_OPTIMIZE_LEVEL == 3, "expected optimize level == 3");
+assert(ASC_SHRINK_LEVEL == 1, "expected shrink level == 1");
+assert(ASC_FEATURE_SIMD, "expected SIMD enabled");
