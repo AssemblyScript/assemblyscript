f8308f8e90e986dcbd4bcb1610b52cc929a3c69a
MaxGraey <maxgraey@gmail.com>
2020-08-21 12:40:52 +0300

    more

--- std/assembly/util/uri.ts
+++ std/assembly/util/uri.ts
@@ -8,7 +8,8 @@
 
 import { CharCode } from "./string";
 
-const URI_SAFE = memory.data<u8>([
+// @ts-ignore: decorator
+@lazy const URI_SAFE = memory.data<u8>([
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -27,7 +28,8 @@ const URI_SAFE = memory.data<u8>([
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ]);
 
-const URL_SAFE = memory.data<u8>([
+// @ts-ignore: decorator
+@lazy const URL_SAFE = memory.data<u8>([
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
@@ -46,30 +48,30 @@ const URL_SAFE = memory.data<u8>([
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ]);
 
-export function escapeUnsafe(dest: usize, source: usize, table: u8[], len: isize): bool {
-  const HEX_CHARS: u8[] = [
+export function escapeUnsafe(dst: usize, src: usize, len: isize, table: usize): bool {
+  const HEX_CHARS = memory.data<u8>([
     0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
     0x41, 0x42, 0x43, 0x44, 0x45, 0x46
-  ];
+  ]);
 
   var hex0: u16 = CharCode.PERCENT; // %
   var i: isize = 0, org: isize, offset: usize = 0;
   while (i < len) {
     org = i;
-    let ch = load<u16>(source + (i << 1));
-    while (i < len && ch <= 127 && table[ch] != 0) {
+    let ch = <u32>load<u16>(src + (i << 1));
+    while (i < len && ch <= 127 && load<u8>(table + ch) != 0) {
       ++i;
-      ch = load<u16>(source + (i << 1));
+      ch = <u32>load<u16>(src + (i << 1));
     }
     if (i > org) {
       if (!org) {
         if (i >= len) return false;
-        // reallocUnsafe(dest, len * 12 / 10);
+        dst = __realloc(dst, len * 12 / 10);
       }
       let size = <usize>(i - org) << 1;
       memory.copy(
-        dest   + offset,
-        source + <usize>(org << 1),
+        dst + offset,
+        src + <usize>(org << 1),
         size
       );
       offset += size;
@@ -77,12 +79,37 @@ export function escapeUnsafe(dest: usize, source: usize, table: u8[], len: isize
     /* escaping */
     if (i >= len) break;
 
-    ch = load<u16>(source + (i << 1));
-    let hex1 = <u32>HEX_CHARS[(ch >>> 4) & 0x0F] | (<u32>HEX_CHARS[ch & 0x0F] << 16);
-    store<u16>(dest + offset, hex0, 0);
-    store<u32>(dest + offset, hex1, 2);
+    ch = <u32>load<u16>(src + (i << 1));
+    let hex1 = (
+      (<u32>load<u8>(HEX_CHARS + ((ch >>> 4) & 0x0F))) |
+      (<u32>load<u8>(HEX_CHARS + ((ch >>> 0) & 0x0F)) << 16)
+    );
+    store<u16>(dst + offset, hex0, 0);
+    store<u32>(dst + offset, hex1, 2);
     offset += 3 << 1;
     ++i;
   }
   return true;
+}
+
+export function escapeURI(str: string): string | null {
+  var len = str.length;
+  if (!len) return str;
+  var result = __alloc(1, idof<string>());
+  if (!escapeUnsafe(result, changetype<usize>(str), len, URI_SAFE)) {
+    __free(result);
+    return null;
+  }
+  return changetype<string>(result); // retain
+}
+
+export function escapeURIComponent(str: string): string | null {
+  var len = str.length;
+  if (!len) return str;
+  var result = __alloc(1, idof<string>());
+  if (!escapeUnsafe(result, changetype<usize>(str), len, URL_SAFE)) {
+    __free(result);
+    return null;
+  }
+  return changetype<string>(result); // retain
 }
