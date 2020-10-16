b260be70d9782f289ed9cee3d696981de442419c
Martin Fredriksson <martin.fredriksson@vikinganalytics.se>
2020-10-16 12:54:57 +0200

    Changed to Imports[key: string]: Record<string,unknown>|undefined

--- lib/loader/index.d.ts
+++ lib/loader/index.d.ts
@@ -8,7 +8,7 @@ export interface ResultObject {
 
 /** WebAssembly imports with an optional env object and two levels of nesting. */
 export type Imports = {
-  [key: string]: Record<string,unknown> | any;  /** env?: {} not allowed without the '| any' */
+  [key: string]: Record<string,unknown> | undefined;  /** env?: {} not allowed without the '| undefined' anymore */
   env?: {
     memory?: WebAssembly.Memory;
     table?: WebAssembly.Table;
