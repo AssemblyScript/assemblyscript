56fb0fdc2063b4a21f31cfbabbb00996f268e1a9
dcode <dcode@dcode.io>
2020-10-22 10:39:28 +0200

    Make loader ESM by default, with UMD fallback

--- .eslintrc.js
+++ .eslintrc.js
@@ -92,9 +92,11 @@ module.exports = {
         "bin/*"
       ],
       rules: {
-        // Node's support for ESM is still not great, but this rule is likely
-        // to become activated once compatibility doesn't suck anymore.
+        // We are testing both ESM and UMD, so don't limit us.
         "@typescript-eslint/no-var-requires": "off",
+
+        // This rule does not behave well in JS files.
+        "@typescript-eslint/explicit-module-boundary-types": "off",
 
         // Enforcing to remove function parameters on stubs makes code less
         // maintainable, so we instead allow unused function parameters.
