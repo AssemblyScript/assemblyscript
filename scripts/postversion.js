e9ead0b149d205f4f3c11450976e8fa16c2ad090
dcode <dcode@dcode.io>
2020-10-25 22:22:56 +0100

    do a test-run

--- scripts/postversion.js
+++ scripts/postversion.js
@@ -1,19 +1,5 @@
-const fs = require("fs");
-const path = require("path");
-
 // Make sure that we are not publishing a v1 just yet
 const mainPkg = require("../package.json");
 if (!/^0\./.test(mainPkg.version)) {
   throw Error("Unexpected version: " + mainPkg.version);
 }
-
-// Sync lib with main package
-[
-  "lib/loader",
-  "lib/rtrace"
-].forEach(dir => {
-  const pkgFile = path.join(__dirname, "..", dir, "package.json");
-  const pkg = require(pkgFile);
-  pkg.version = mainPkg.version;
-  fs.writeFileSync(pkgFile, JSON.stringify(pkg, null, 2));
-});
