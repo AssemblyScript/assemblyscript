0211ed740237adf388096508c92a1a64fe9913aa
Carl <bionitsoup@gmail.com>
2021-01-11 04:15:40 +0700

    Fixed indentation for wrap method.

--- src/definitions.ts
+++ src/definitions.ts
@@ -496,7 +496,8 @@ export class TSDBuilder extends ExportsWalker {
       sb.push(extendsNode.name.identifier.text); // TODO: fqn?
     }
     sb.push(" {\n");
-    sb.push("  static wrap(pointer: usize): ");
+    indent(sb, this.indentLevel);
+    sb.push("static wrap(pointer: usize): ");
     sb.push(name);
     sb.push("\n");
     var staticMembers = element.prototype.members;
