69f04643eb065d04215985b72413b46dc91cd143
dcode <dcode@dcode.io>
2021-02-03 20:01:44 +0100

    Support optional parameters in d.ts generation

--- src/definitions.ts
+++ src/definitions.ts
@@ -459,11 +459,11 @@ export class TSDBuilder extends ExportsWalker {
     sb.push("(");
     var parameters = signature.parameterTypes;
     var numParameters = parameters.length;
-    // var requiredParameters = signature.requiredParameters;
+    var requiredParameters = signature.requiredParameters;
     for (let i = 0; i < numParameters; ++i) {
       if (i) sb.push(", ");
-      // if (i >= requiredParameters) sb.push("optional ");
       sb.push(element.getParameterName(i));
+      if (i >= requiredParameters) sb.push("?");
       sb.push(": ");
       sb.push(this.typeToString(parameters[i]));
     }
