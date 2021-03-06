6a6cc92f562b42168f0e7c211a610220134df235
MaxGraey <maxgraey@gmail.com>
2021-03-06 21:25:26 +0200

    use unsigned shifts for bswap

--- std/assembly/polyfills.ts
+++ std/assembly/polyfills.ts
@@ -1,12 +1,12 @@
 export function bswap<T extends number>(value: T): T {
   if (isInteger<T>()) {
     if (sizeof<T>() == 2) {
-      return <T>((value << 8) | ((value >> 8) & <T>0x00FF));
+      return <T>((value << 8) | ((value >>> 8) & <T>0x00FF));
     }
     if (sizeof<T>() == 4) {
       return <T>(
-        rotl<u32>(<u32>value & 0xFF00FF00, 8) |
-        rotr<u32>(<u32>value & 0x00FF00FF, 8)
+        rotl<u32>(value & 0xFF00FF00, 8) |
+        rotr<u32>(value & 0x00FF00FF, 8)
       );
     }
     if (sizeof<T>() == 8) {
@@ -28,9 +28,9 @@ export function bswap<T extends number>(value: T): T {
 export function bswap16<T extends number>(value: T): T {
   if (isInteger<T>() && sizeof<T>() <= 4) {
     if (sizeof<T>() == 2) {
-      return <T>((value << 8) | ((value >> 8) & <T>0x00FF));
+      return <T>((value << 8) | ((value >>> 8) & <T>0x00FF));
     } else if (sizeof<T>() == 4) {
-      return <T>(((value << 8) & <T>0xFF00) | ((value >> 8) & <T>0x00FF) | (value & <T>0xFFFF0000));
+      return <T>(((value << 8) & <T>0xFF00) | ((value >>> 8) & <T>0x00FF) | (value & <T>0xFFFF0000));
     }
     return value;
   }
