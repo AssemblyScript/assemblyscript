6b01c7d081cfc384c38e345c274d667d0212a950
MaxGraey <maxgraey@gmail.com>
2021-03-17 14:23:16 +0200

    store utf8 code points

--- std/assembly/util/uri.ts
+++ std/assembly/util/uri.ts
@@ -5,7 +5,7 @@
 import { CharCode } from "./string";
 
 // @ts-ignore: decorator
-@lazy export const URI_SAFE = memory.data<u8>([
+@lazy const URI_SAFE = memory.data<u8>([
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
@@ -17,7 +17,7 @@ import { CharCode } from "./string";
 ]);
 
 // @ts-ignore: decorator
-@lazy export const URL_SAFE = memory.data<u8>([
+@lazy const URL_SAFE = memory.data<u8>([
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0,
@@ -28,23 +28,48 @@ import { CharCode } from "./string";
   1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0
 ]);
 
+// @ts-ignore: decorator
+@lazy const HEX_CHARS = memory.data<u8>([
+  0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
+  0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46
+]);
+
+function encodeHexPoint(dst: usize, offset: usize, c: u32): isize {
+  const U: u16 = 0x75; // u
+  const P: u16 = CharCode.PERCENT; // %
+
+  store<u16>(dst + offset, P); // %
+  offset += 1 << 1;
+  if (c >= 0xFF) {
+    store<u16>(dst + offset, U); // u
+    let hex = (
+      (<u32>load<u8>(HEX_CHARS + ((c >>> 12) & 0x0F))) |
+      (<u32>load<u8>(HEX_CHARS + ((c >>>  8) & 0x0F)) << 16)
+    );
+    store<u32>(dst + offset, hex, 2); // XX
+    offset += 3 << 1;
+  }
+  let hex = (
+    (<u32>load<u8>(HEX_CHARS + ((c >>> 4) & 0x0F))) |
+    (<u32>load<u8>(HEX_CHARS + ((c >>> 0) & 0x0F)) << 16)
+  );
+  store<u32>(dst + offset, hex); // XX
+  offset += 2 << 1;
+  return offset;
+}
+
 export function escapeUnsafe(dst: usize, src: usize, len: isize, table: usize): bool {
-  const HEX_CHARS = memory.data<u8>([
-    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37,
-    0x38, 0x39, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46
-  ]);
-  const h0: u16 = CharCode.PERCENT; // %
   var i: isize = 0, org: isize, offset: usize = 0;
   while (i < len) {
     org = i;
-    let ch: u32;
-    while (i < len) {
-      ch = <u32>load<u16>(src + (i << 1));
-      if (ch <= 127) {
-        if (!load<u8>(table + ch)) break;
-      } else if (ch >= 0xD800) break;
+    let c: u32, c1: u32;
+    do {
+      c = <u32>load<u16>(src + (i << 1));
+      if (c <= 127) {
+        if (!load<u8>(table + c)) break;
+      } else break;
       ++i;
-    }
+    } while (i < len);
 
     if (i > org) {
       if (!org) {
@@ -59,18 +84,37 @@ export function escapeUnsafe(dst: usize, src: usize, len: isize, table: usize): 
       );
       offset += size;
     }
-    /* escaping */
-    if (i >= len) break;
 
-    ch = <u32>load<u16>(src + (i << 1));
-    let h1 = (
-      (<u32>load<u8>(HEX_CHARS + ((ch >>> 4) & 0x0F))) |
-      (<u32>load<u8>(HEX_CHARS + ((ch >>> 0) & 0x0F)) << 16)
-    );
-    store<u16>(dst + offset, h0, 0);
-    store<u32>(dst + offset, h1, 2);
-    offset += 3 << 1;
-    ++i;
+    if (c >= 0xDC00 && c <= 0xDFFF) {
+      // throw new Error("invalid character");
+      return false;
+    }
+
+    if (c >= 0xD800 && c <= 0xDBFF) {
+      c1 = <u32>load<u16>(src + (i << 1));
+      ++i;
+      if (c1 < 0xDC00 || c1 > 0xDFFF) {
+        // throw new Error("expecting surrogate pair");
+        return false;
+      }
+      c = (((c & 0x3FF) << 10) | (c1 & 0x3FF)) + 0x10000;
+    }
+
+    if (c <= 0x7F) {
+      offset += encodeHexPoint(dst, offset, c);
+    } else {
+      if (c <= 0x800) {
+        offset += encodeHexPoint(dst, offset, (c >> 6) | 0xC0);
+      } else {
+        if (c < 0x10000) {
+          offset += encodeHexPoint(dst, offset, (c >> 12) | 0xE0);
+        } else {
+          offset += encodeHexPoint(dst, offset, (c >> 18) | 0xF0);
+          offset += encodeHexPoint(dst, offset, ((c >> 12) & 0x3F) | 0x80);
+        }
+        offset += encodeHexPoint(dst, offset, ((c >> 6) & 0x3F) | 0x80);
+      }
+    }
   }
   return true;
 }
