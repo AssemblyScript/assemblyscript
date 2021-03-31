4c85c58059c1e24e71d7e42c527c8aa24e8daff2
dcode <dcode@dcode.io>
2021-03-31 11:14:09 +0200

    update related asconfig test

--- tests/asconfig/complicated/assembly/index.ts
+++ tests/asconfig/complicated/assembly/index.ts
@@ -1,5 +1,5 @@
 assert(ASC_OPTIMIZE_LEVEL == 3, "expected optimize level == 3");
-assert(ASC_SHRINK_LEVEL == 1, "expected shrink level == 1");
+assert(ASC_SHRINK_LEVEL == 0, "expected shrink level == 0");
 assert(ASC_FEATURE_SIMD, "expected SIMD enabled");
 let size = memory.size();
 trace("size", 1, size);
