f5f7466ad787995d2c782bb5ea8035ce332ea744
Barak Ben Noon <bn.barak@gmail.com>
2021-04-13 14:22:37 -0400

    recreate array-destructuring.untouched.wat

--- tests/compiler/array-destructuring.ts
+++ tests/compiler/array-destructuring.ts
@@ -1,7 +1,7 @@
 const x = 999;
 const y = 888;
 let funcRunCount = 0;
-// function func(): i32[] { funcRunCount++; return [x, y]; };
+function func(): i32[] { funcRunCount++; return [x, y]; };
 // const [x_copy, y_copy] = func();
 // assert(funcRunCount == 1);
 // assert(x_copy + y_copy == x + y);
