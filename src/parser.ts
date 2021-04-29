88c7d2280218f247d6e66f75036beecd69d65ae8
yjhmelody <465402634@qq.com>
2021-04-29 10:52:07 +0800

    fix: internalPath  in parseFile is not same with Source

--- src/parser.ts
+++ src/parser.ts
@@ -136,7 +136,7 @@ export class Parser extends DiagnosticEmitter {
   ): void {
     // the frontend gives us paths with file extensions
     var normalizedPath = normalizePath(path);
-    var internalPath = mangleInternalPath(path);
+    var internalPath = mangleInternalPath(normalizedPath);
 
     // check if already processed
     if (this.donelog.has(internalPath)) return;
