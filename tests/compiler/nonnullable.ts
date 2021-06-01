9810f2fdd81e8b2f5749bb352233f7ac29ebb280
Willem Wyndham <willem@cs.umd.edu>
2021-06-01 11:19:23 -0400

    nit: simplify test

--- tests/compiler/nonnullable.ts
+++ tests/compiler/nonnullable.ts
@@ -1,31 +1,13 @@
 
 
+type nullableString = string | null;;
 
 assert(nameof<NonNullable<u32>>() == nameof<u32>());
-
+assert(nameof<NonNullable<nullableString>>() == nameof<string>());
 assert(nameof<NonNullable<string[] | null>>() == nameof<string[]>());
 
-function takesGeneric<T>(t: T): void {
-  if (!isNullable<T>() || t != null){
-    // @ts-ignore
-    if  (isDefined(t.call)){
-      t.call();
-    }
-  }
+function assertNonNull<T>(): void {
+  assert(!isNullable<T>(), "T cannot be null");
 }
 
-function callsGeneric<T>(t: T): void {
-  if (t != null) {
-    takesGeneric<NonNullable<T>>(t); // Would be nice to have a `notNull<T>` here
-  }
-}
-
-class Tee {
-  call(): void {
-    let x: i8 = 0;
-  }
-}
-
-let y: Tee | null = new Tee();
-
-callsGeneric(y);
+assertNonNull<NonNullable<nullableString>>();
