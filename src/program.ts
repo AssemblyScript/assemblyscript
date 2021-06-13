d6a4c525f64336ce490cce7f86f2f7055ae450f7
pitust <piotr@stelmaszek.com>
2021-06-13 02:10:05 +0100

    undo accidental change

--- src/program.ts
+++ src/program.ts
@@ -649,7 +649,6 @@ export class Program extends DiagnosticEmitter {
 
   /** Gets the standard `abort` instance, if not explicitly disabled. */
   get abortInstance(): Function | null {
-    if (this.options.trapAbort) return null
     var prototype = this.lookup(CommonNames.abort);
     if (!prototype || prototype.kind != ElementKind.FUNCTION_PROTOTYPE) return null;
     return this.resolver.resolveFunction(<FunctionPrototype>prototype, null);
