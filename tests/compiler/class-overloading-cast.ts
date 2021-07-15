d5015ebdef354cd383def5a35486d0506ad6ad9c
Willem Wyndham <willem@cs.umd.edu>
2021-07-15 14:54:10 -0400

    Update tests/compiler/class-overloading-cast.ts
    Co-authored-by: Max Graey <maxgraey@gmail.com>

--- tests/compiler/class-overloading-cast.ts
+++ tests/compiler/class-overloading-cast.ts
@@ -12,7 +12,7 @@ class B<T, V> extends A<T> {
 class C extends A<string>{}
 
 let v = new B<i32, f64>();
-let v2 = new B<i32,string>();
+let v2 = new B<i32, string>();
 let v3 = new B<f64, string>();
 assert((<A<i32>>v).foo(1) == "B");
 assert(v2.foo(1) == "B");
