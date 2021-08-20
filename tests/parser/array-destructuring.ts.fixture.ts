76ecd81c6575e91d4196b85b970c894ab31724a3
romdotdog <70765447+romdotdog@users.noreply.github.com>
2021-08-19 23:25:13 -0500

    add more tests to parser

--- tests/parser/array-destructuring.ts.fixture.ts
+++ tests/parser/array-destructuring.ts.fixture.ts
@@ -3,4 +3,12 @@ function test1(): void {
   const {d, e} = f;
   const [] = g;
   const [, ] = h;
+  const [, , , , ] = k;
+  let [] = n;
+  var [, _, ] = j;
+  var [, , , a, , f, , g, g, ] = 1;
+  var [n, a, a, a, v, , h, h, h, h, h, , r, , r, , h, y, ] = 2;
+  let {aaa, a} = 1;
+  var {f, , , , , , , , , , , , } = 3;
+  const {fff} = 3;
 }
