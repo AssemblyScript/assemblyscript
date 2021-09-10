3dd26a04d6200628099e610a99c0138a6e917f1f
dcode <dcode@dcode.io>
2021-09-10 19:38:45 +0200

    Allow void ternary expressions

--- tests/compiler/ternary.ts
+++ tests/compiler/ternary.ts
@@ -11,3 +11,15 @@ a = (0 ? unreachable() : 1) ? 1 : unreachable();
 export function test(x: i32, y: i32, z: i32): i32 {
   return x ? y : z;
 }
+
+export function testDropWithTypeMismatch(cond: bool): void {
+  var x = 1;
+  var y = "a";
+  cond ? x : y;
+}
+
+export function testVoidInclTypeMismatch(cond: bool, nop: () => void): void {
+  cond ? nop() : nop();
+  cond ? nop() : true;
+  cond ? true : nop();
+}
