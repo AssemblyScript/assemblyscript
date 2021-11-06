488101bf9646ed35d6f9c67de94e9f1aac75301a
MaxGraey <maxgraey@gmail.com>
2021-11-06 21:34:55 +0200

    update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 try { require("source-map-support").install(); } catch (e) { /* nop */ }
 require("ts-node").register({
   project: require("path").join(__dirname, "src", "tsconfig.json"),
-  skipIgnore: true
+  skipIgnore: true,
+  files: true
 });
 require("./src/glue/js");
 module.exports = require("./src");
