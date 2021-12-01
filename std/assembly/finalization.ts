61c9d99cbf274837aaaffb552015d2809cf6db7c
Josh Tenner <jtenner@benecare.com>
2021-12-01 12:47:35 -0500

    code style, multiple reference registries

--- std/assembly/finalization.ts
+++ std/assembly/finalization.ts
@@ -5,79 +5,93 @@
  * @param {usize} ptr - the pointer being finalized.
  */
 
-// @ts-ignore: global
+// @ts-ignore: decorators
 @global @lazy
 export function __finalize(ptr: usize): void {
-
-    // then check to see if a reference is located on a registry
+    // check to see if this pointer has any registries associated with it
     if (finalizationRegistryMap.has(ptr)) {
-        // get the registry associated
-        let registry = finalizationRegistryMap.get(ptr);
 
-        // virtual call and handle the pointer
-        registry.handle(ptr);
+      // loop over each one and call the handle callback
+      let registries = finalizationRegistryMap.get(ptr);
+      let length = registries.length;
+      for (let i = 0; i < length; i++) {
+        let registry = unchecked(registries[i]);
 
-        // remove it from the registry
-        finalizationRegistryMap.delete(ptr);
+        // let the registry handle it with a virtual call
+        registry.__handle(ptr);
+      }
+
+      // remove it from the registry
+      finalizationRegistryMap.delete(ptr);
     }
 }
 
 /** This map is used to keep track of the callback index used. */
 // @ts-ignore: lazy
-@lazy let finalizationRegistryMap = new Map<usize, BaseFinalizationRegistry>();
+@lazy let finalizationRegistryMap = new Map<usize, BaseFinalizationRegistry[]>();
 
 
 /** Dummy base class to help pin it with the `pinned` `Set`. */
 abstract class BaseFinalizationRegistry {
-  constructor() {}
 
   /** Just a virtual call handle for handle(ptr: usize). */
-  abstract handle(ptr: usize): void;
+  abstract __handle(ptr: usize): void;
 }
 
 /** A finalization Registry object that contains targets and held values. */
 export class FinalizationRegistry<TTarget, THeldValue> extends BaseFinalizationRegistry {
-  private held = new Map<TTarget, THeldValue>();
+  /**
+   * This holds the held references by pointer. It cannot be by reference because
+   * references won't be cleaned up, and it causes a memory leak.
+   */
+  private held = new Map<usize, THeldValue>();
 
   constructor(
-      /** The callback for the registry */
-      public callback: (ref: TTarget, data: THeldValue) => void) {
+    /** The callback for the registry */
+    public callback: (data: THeldValue) => void
+  ) {
     // Set the reference registry callback
     super();
   }
 
-  /**
-   * Register a cleanup target.
-   *
-   * @param {TTarget} target - The target to be registered.
-   * @param {THeldValue} held - The held value to be given to the registered callback
-   * when the reference is cleaned up.
-   */
-  register(target: TTarget, held: THeldValue): void {
-      // the specification requires that target cannot be held
-      if (isReference(held)) {
-          assert(changetype<usize>(target) != changetype<usize>(held));
-      }
+  /** Register a cleanup target. */
+  register(
+    /** The target to be registered. */
+    target: TTarget,
+    /** The held value to be given to the registered callback */
+    held: THeldValue
+  ): void {
+    let ptr = changetype<usize>(target);
+    // the specification requires that target cannot be held
+    if (isReference(held)) {
+      assert(ptr != changetype<usize>(held));
+    }
 
-      // set the registry map
-      finalizationRegistryMap.set(changetype<usize>(target), this);
-      // hold the held value
-      this.held.set(target, held);
+    // references can belong to multiple registries
+    if (!finalizationRegistryMap.has(ptr)) {
+      finalizationRegistryMap.set(ptr, [this]);
+    } else {
+      let registryList = finalizationRegistryMap.get(ptr);
+      if (!registryList.includes(this)) registryList.push(this);
+    }
+
+    // hold the held value
+    this.held.set(ptr, held);
   }
 
   /**
    * This method is used as an instance method for call_redirect to enable
    * handling registry callbacks.
-   *
-   * @param {TTarget} ref - The reference passed by the __finalize callback.
    */
-  handle(refPtr: usize): void {
-    let ref = changetype<TTarget>(refPtr);
+  @unsafe __handle(
+    /**  The reference passed by the __finalize callback. */
+    refPtr: usize
+  ): void {
     // obtain the held value
-    let held = this.held.get(ref);
+    let held = this.held.get(refPtr);
     // call the callback with the ref and the held value
-    this.callback(ref, held);
+    this.callback(held);
     // delete the held value
-    this.held.delete(ref);
+    this.held.delete(refPtr);
   }
 }
