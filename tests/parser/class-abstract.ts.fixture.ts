54a1d20a37f3c98349584d0bdda36a8e000b09b0
romdotdog <70765447+romdotdog@users.noreply.github.com>
2021-12-05 22:22:10 -0600

    new fixtures for new error in class-abstract test

--- tests/parser/class-abstract.ts.fixture.ts
+++ tests/parser/class-abstract.ts.fixture.ts
@@ -1,5 +1,5 @@
 class Foo {
   bar(): void;
 }
-// ERROR 1042: "'abstract' modifier cannot be used here." in class-abstract.ts(2,3+8)
+// ERROR 1242: "'abstract' modifier can only appear on a class, method, or property declaration." in class-abstract.ts(2,3+8)
 // ERROR 2391: "Function implementation is missing or not immediately following the declaration." in class-abstract.ts(2,19+4)
