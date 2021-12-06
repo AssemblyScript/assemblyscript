9fab45e2252a15861b55994c607b3098260674f8
MaxGraey <maxgraey@gmail.com>
2021-12-06 12:03:38 +0200

    fix

--- tests/parser/constructor.ts.fixture.ts
+++ tests/parser/constructor.ts.fixture.ts
@@ -3,5 +3,6 @@ class MyClass {
   constructor(a: i32) {}
   constructor(a: i32, b: i32) {}
 }
-class MyClassImplicit {}
-// ERROR 1005: "')' expected." in constructor.ts(8,30+7)
+class MyClassImplicit {
+  constructor(public a: i32, private readonly b: i32 = 2, c: i32 = 3) {}
+}
