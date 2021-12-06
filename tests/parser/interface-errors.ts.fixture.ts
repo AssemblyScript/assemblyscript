167f97f0156e0e3edc6a7cc37c62cd52e2e8a4ac
romdotdog <70765447+romdotdog@users.noreply.github.com>
2021-12-06 00:05:11 -0600

    temporarily update interface-errors fixture

--- tests/parser/interface-errors.ts.fixture.ts
+++ tests/parser/interface-errors.ts.fixture.ts
@@ -9,7 +9,7 @@ interface Foo {
 interface Foo extends Bar {
 }
 interface Foo {
-  a: i32;
+  abstract a: i32;
   b: i32;
   c: i32;
   c: i32;
@@ -21,6 +21,7 @@ interface Foo {
 // ERROR 1242: "'abstract' modifier can only appear on a class, method, or property declaration." in interface-errors.ts(1,1+8)
 // ERROR 1176: "Interface declaration cannot have 'implements' clause." in interface-errors.ts(8,15+10)
 // ERROR 1070: "'abstract' modifier cannot appear on a type member." in interface-errors.ts(15,3+8)
+// ERROR 100: "Not implemented: Abstract fields" in interface-errors.ts(15,12+1)
 // ERROR 1070: "'private' modifier cannot appear on a type member." in interface-errors.ts(16,3+7)
 // ERROR 1070: "'protected' modifier cannot appear on a type member." in interface-errors.ts(17,3+9)
 // ERROR 1070: "'public' modifier cannot appear on a type member." in interface-errors.ts(18,3+6)
