fb6bc8777b25eae88efbb0952cc0dec346211388
dcode <dcode@dcode.io>
2022-01-10 20:51:38 +0100

    some Deno compatibility for asc

--- bin/asc.js
+++ bin/asc.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
-import childProcess from "child_process";
-
 const [ nodePath, thisPath, ...args ] = process.argv;
 const nodeArgs = process.execArgv;
 
 const hasSourceMaps = nodeArgs.includes("--enable-source-maps");
 const posCustomArgs = args.indexOf("--");
+const isDeno = typeof Deno !== "undefined";
 
-if (!hasSourceMaps || ~posCustomArgs) {
+if (isDeno) {
+  process.on = function() { /* suppress 'not implemented' message */ };
+}
+
+if ((!hasSourceMaps || ~posCustomArgs) && !isDeno) {
   if (!hasSourceMaps) {
     nodeArgs.push("--enable-source-maps");
   }
@@ -16,14 +19,13 @@ if (!hasSourceMaps || ~posCustomArgs) {
     nodeArgs.push(...args.slice(posCustomArgs + 1));
     args.length = posCustomArgs;
   }
-  childProcess.spawnSync(
+  (await import("child_process")).spawnSync(
     nodePath,
     [...nodeArgs, thisPath, ...args],
     { stdio: "inherit" }
   );
 } else {
-  const asc = await import("../dist/asc.js");
-  const { error } = await asc.main(process.argv.slice(2), {
+  const { error } = (await import("../dist/asc.js")).main(process.argv.slice(2), {
     stdout: process.stdout,
     stderr: process.stderr
   });
