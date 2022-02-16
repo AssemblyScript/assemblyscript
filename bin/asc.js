08dcfbc76269c9c135c6844a86993967bd599f38
Joe Pea <joe.pea@nasa.gov>
2022-02-15 17:39:59 -0800

    improve variable name

--- bin/asc.js
+++ bin/asc.js
@@ -4,20 +4,20 @@ const [ nodePath, thisPath, ...args ] = process.argv;
 const nodeArgs = process.execArgv;
 
 const hasSourceMaps = nodeArgs.includes("--enable-source-maps");
-const posCustomArgs = args.indexOf("--");
+const customNodeArgs = args.indexOf("--");
 const isDeno = typeof Deno !== "undefined";
 
 if (isDeno) {
   process.on = function() { /* suppress 'not implemented' message */ };
 }
 
-if ((!hasSourceMaps || ~posCustomArgs) && !isDeno) {
+if ((!hasSourceMaps || ~customNodeArgs) && !isDeno) {
   if (!hasSourceMaps) {
     nodeArgs.push("--enable-source-maps");
   }
-  if (~posCustomArgs) {
-    nodeArgs.push(...args.slice(posCustomArgs + 1));
-    args.length = posCustomArgs;
+  if (~customNodeArgs) {
+    nodeArgs.push(...args.slice(customNodeArgs + 1));
+    args.length = customNodeArgs;
   }
   (await import("child_process")).spawnSync(
     nodePath,
