af5bd953db4fba2b12b66c0308b9563f436a7af2
Jesse <xiang19890319@gmail.com>
2022-05-19 17:52:51 +0800

    Enhancement: optimize the test for function parameter parser
    Signed-off-by: jesse <xiang19890319@gmail.com>

--- tests/parser/function.ts
+++ tests/parser/function.ts
@@ -4,3 +4,18 @@ function typeparams<T, V extends T>(a: V | null = null): void {}
 function withdecorator(): void {}
 function withthis(this: i32): i32 { return this; }
 function withthisp(this: i32, a: f32, b: f64): i32 { return this; }
+
+// 1003: Identifier expected.
+function typevoid<T extends () => void>():void{}
+
+// 1003: Identifier expected.
+function extendsfunctiontype(this: ()=>void):void {}
+
+// 1005: ':' expected.
+function extendsinsteadofcolon(this extends i32):void {}
+
+// 1003: Identifier expected.
+function typeNone<void>():void{}
+
+// 1005: ')' expected.
+function functionforgetcomma(this: ()=>void a: i32):void {}
--- tests/parser/function.ts.fixture.ts
+++ tests/parser/function.ts.fixture.ts
@@ -8,3 +8,10 @@ function withthis(this: i32): i32 {
 function withthisp(this: i32, a: f32, b: f64): i32 {
   return this;
 }
+function extendsfunctiontype(): void {}
+// ERROR 1003: "Identifier expected." in function.ts(9,29+10)
+// ERROR 1003: "Identifier expected." in function.ts(13,36+8)
+// ERROR 1005: "':' expected." in function.ts(16,32+4)
+// ERROR 1003: "Identifier expected." in function.ts(19,19+4)
+// ERROR 1003: "Identifier expected." in function.ts(22,36+8)
+// ERROR 1005: "')' expected." in function.ts(22,40+4)
