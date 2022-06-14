011bc1615100b15f91dc3b2409449bf4db9c521f
romdotdog <70765447+romdotdog@users.noreply.github.com>
2022-06-14 00:23:26 -0500

    fix ast serialization for real this time

--- src/extra/ast.ts
+++ src/extra/ast.ts
@@ -1487,7 +1487,7 @@ export class ASTBuilder {
   }
 
   visitVariableDeclaration(node: VariableDeclaration): void {
-    this.visitNode(node);
+    this.visitNode(node.name);
     var type = node.type;
     var sb = this.sb;
     if (node.flags & CommonFlags.DEFINITELY_ASSIGNED) {
