a5492b7673f535aac6b8f99d36b1a741228324d6
ANDO Yasushi <andyjpn@gmail.com>
2022-07-02 23:27:13 +0900

    add getter/setter tests

---
+++ tests/compiler/object-literal-method-errors.ts
@@ -0,0 +1,13 @@
+class SomeClass {
+  val: i32
+}
+
+const obj = {
+  get val(): i32 { // AS100: Not implemented: Getter/setter in object literal
+    return 42 as i32;
+  },
+  set val(v: i32) { // AS100: Not implemented: Getter/setter in object literal
+  }
+} as SomeClass;
+
+ERROR("EOF");
--- tests/parser/object-literal-method.ts.fixture.ts
+++ tests/parser/object-literal-method.ts.fixture.ts
@@ -1,5 +1,9 @@
 const obj = {
   add(a: i32, b: i32): i32 {
     return a + b;
-  }
+  },
+  get val(): i32 {
+    return 42;
+  },
+  set val(v: i32) {}
 };
