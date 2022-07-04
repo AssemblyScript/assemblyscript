59c4392f1f620f51fafe86a074015c473a8ca159
romdotdog <70765447+romdotdog@users.noreply.github.com>
2022-07-04 11:13:42 -0500

    add more tests

--- tests/compiler/void.ts
+++ tests/compiler/void.ts
@@ -10,3 +10,14 @@ var u8Val1: u8 = 1;
 var u8Val2: u8 = 255;
 
 void (u8Val1 + u8Val2); // can remain unwrapped
+
+var foo = -1;
+[1, 2, 3, 4, 5, 6, 7, 8].forEach(v => void (foo = v));
+assert(foo == 8);
+
+function aVoid(): void {
+  return void (foo = 9);
+}
+
+aVoid();
+assert(foo == 9);
