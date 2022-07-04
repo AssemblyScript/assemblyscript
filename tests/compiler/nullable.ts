bbc3fe1a38d18fdc3519267d69635ffb9226d704
Congcong Cai <congcongcai0907@163.com>
2022-07-04 09:14:01 +0800

    test: add test case for #2359

--- tests/compiler/nullable.ts
+++ tests/compiler/nullable.ts
@@ -4,4 +4,14 @@ function notNullable(a: Example): void {}
 
 notNullable(null);
 
+export function test(): void {
+  let value: Example | null = new Example();
+  if (value != null) {
+    // value = null;
+    true && (value = null);
+    // "TS2322: Type 'nullable/Example | null' is not assignable to type 'nullable/Example'.",
+    notNullable(value);
+  }
+}
+
 ERROR("EOF");
