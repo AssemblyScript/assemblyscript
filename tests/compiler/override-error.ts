c2c1e355415768d287668f4764b29e4de1b46f58
Congcong Cai <congcongcai0907@163.com>
2022-07-13 22:01:58 +0800

    update stderr

--- tests/compiler/override-error.ts
+++ tests/compiler/override-error.ts
@@ -4,7 +4,7 @@ class A {
 
 class B extends A {
   override method(): void {}
-  // This member cannot have an 'override' modifier because it is not declared in the base class 'A'.
+  // TS4117: This member cannot have an 'override' modifier because it is not declared in the base class 'A'.
   override method_error(): void {}
 }
 
