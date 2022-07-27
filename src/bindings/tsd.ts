3b615cbdda454cb61ac1d0f5b8baf13313dd0f28
Congcong Cai (EE-CN-42) <Congcong.CA.Cai@bmw.com>
2022-07-27 14:16:25 +0800

    fix: fix endless recursion during dts generation

--- src/bindings/tsd.ts
+++ src/bindings/tsd.ts
@@ -302,17 +302,21 @@ export class TSDBuilder extends ExportsWalker {
             sb.push(mode == Mode.EXPORT ? "<never>" : "<undefined>");
           }
         } else {
-          if (this.isPlainObject(clazz)) {
+          let isPlainObject = this.isPlainObject(clazz);
+          if (isPlainObject) {
             typeName = "__Record" + clazz.id.toString();
             sb.push(typeName);
             sb.push(mode == Mode.EXPORT ? "<never>" : "<undefined>");
-            this.deferredTypings.push(this.makeRecordType(clazz, mode));
           } else {
             typeName = "__Internref" + clazz.id.toString();
             sb.push(typeName);
-            this.deferredTypings.push(this.makeInternrefType(clazz));
           }
           seenObjectTypes.set(clazz, typeName);
+          if (isPlainObject) {
+            this.deferredTypings.push(this.makeRecordType(clazz, mode));
+          } else {
+            this.deferredTypings.push(this.makeInternrefType(clazz));
+          }
         }
       }
       if (type.is(TypeFlags.NULLABLE)) {
