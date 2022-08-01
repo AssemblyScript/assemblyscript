c57bda64e10a0627fa53f8a0e8f82d7ea34df935
MaxGraey <maxgraey@gmail.com>
2022-08-02 00:42:06 +0300

    improve performace.now

--- std/assembly/performance.ts
+++ std/assembly/performance.ts
@@ -1,7 +1,6 @@
 import {
   clock_time_get,
   clockid,
-  errnoToString,
   tempbuf
 } from "bindings/wasi";
 
@@ -12,9 +11,13 @@ import {
 export namespace performance {
   export function now(): f64 {
     if (isDefined(ASC_WASI)) {
-      let err = clock_time_get(clockid.MONOTONIC, 1000, tempbuf); // TODO: more precision?
-      if (err) throw new Error(errnoToString(err));
-      return <f64>load<u64>(tempbuf) / 1000000;
+      let err = clock_time_get(clockid.MONOTONIC, 1000000, tempbuf);
+      if (err) {
+        // try with less precission
+        err = clock_time_get(clockid.MONOTONIC, 1000, tempbuf);
+        if (err) throw new Error("Can't retrive time from clock_time_get");
+      }
+      return <f64>load<u64>(tempbuf) / 1e6;
     } else {
       return performance_binding.now();
     }
