b412c7582f7675c7ec91eac5a8c590ee81268fca
Congcong Cai <congcongcai0907@163.com>
2022-08-06 19:23:44 +0800

    Update src/compiler.ts
    Co-authored-by: Max Graey <maxgraey@gmail.com>

--- src/compiler.ts
+++ src/compiler.ts
@@ -3521,13 +3521,12 @@ export class Compiler extends DiagnosticEmitter {
     var module = this.module;
 
     // void to void
-    if (fromType.kind == TypeKind.VOID && toType.kind == TypeKind.VOID) {
-      return expr;
-    }
-
-    // void to any
     if (fromType.kind == TypeKind.VOID) {
-      assert(toType.kind != TypeKind.VOID); // convertExpression should not be called with void -> void
+      if (toType.kind == TypeKind.VOID) {
+        // void to void
+        return expr;
+      }
+      // void to any
       this.error(
         DiagnosticCode.Type_0_is_not_assignable_to_type_1,
         reportNode.range, fromType.toString(), toType.toString()
