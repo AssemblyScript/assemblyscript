62e0f29b0596cd5b3a07027bcb839f493b529cc9
dcode <dcode@dcode.io>
2022-09-26 00:37:58 +0200

    Add script to generate unicode identifier starts/parts

---
+++ scripts/unicode-identifier.js
@@ -0,0 +1,36 @@
+// see https://github.com/microsoft/TypeScript/blob/main/scripts/regenerate-unicode-identifier-parts.js
+
+const MAX_UNICODE_CODEPOINT = 0x10FFFF;
+const isStart = c => /[\p{ID_Start}\u{2118}\u{212E}\u{309B}\u{309C}]/u.test(c); // Other_ID_Start explicitly included for back compat - see http://www.unicode.org/reports/tr31/#Introduction
+const isPart = c => /[\p{ID_Continue}\u{00B7}\u{0387}\u{19DA}\u{1369}\u{136A}\u{136B}\u{136C}\u{136D}\u{136E}\u{136F}\u{1370}\u{1371}]/u.test(c) || isStart(c); // Likewise for Other_ID_Continue
+const parts = [];
+let partsActive = false;
+let startsActive = false;
+const starts = [];
+
+for (let i = 0; i < MAX_UNICODE_CODEPOINT; i++) {
+  if (isStart(String.fromCodePoint(i)) !== startsActive) {
+    starts.push(i - +startsActive);
+    startsActive = !startsActive;
+  }
+  if (isPart(String.fromCodePoint(i)) !== partsActive) {
+    parts.push(i - +partsActive);
+    partsActive = !partsActive;
+  }
+}
+
+function tablify(arr) {
+  let sb = ["/*\n| from  ...  to | from  ...  to | from  ...  to | from  ...  to |*/\n  "];
+  let i = 0;
+  while (i < arr.length) {
+    let s = arr[i].toString().padStart(6);
+    if (i && !(i % 8)) sb.push("\n  ");
+    sb.push(`${s}, `);
+    ++i;
+  }
+  sb.push("\n");
+  return sb.join("");
+}
+
+console.log(`const unicodeIdentifierStart: u32[] = [${tablify(starts)}];`);
+console.log(`const unicodeIdentifierPart: u32[] = [${tablify(parts)}];`);
