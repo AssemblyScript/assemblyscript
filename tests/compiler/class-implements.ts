3afcce8b0a298d01a364947adfd0d0637ac0f739
JesseCodeBones <56120624+JesseCodeBones@users.noreply.github.com>
2022-10-06 14:56:25 +0800

    fix: Properly handle multiple implemented interfaces (#2510)
    Co-authored-by: dcode <dcode@dcode.io>

--- tests/compiler/class-implements.ts
+++ tests/compiler/class-implements.ts
@@ -2,6 +2,10 @@ interface I {
   foo(): i32;
 }
 
+interface J {
+  foo(): i32;
+}
+
 class A implements I {
   foo(): i32 { return 1; }
 }
@@ -18,3 +22,29 @@ class C extends B implements I {
 
 var c = new C();
 assert(c.foo() == 2);
+
+class D implements I, J {
+  foo(): i32 { return 3; }
+}
+
+let d = new D();
+assert(d.foo() == 3);
+
+class E extends D implements I {
+}
+
+let e = new E();
+assert(e.foo() == 3);
+
+class F extends D implements I {
+  foo(): i32 { return 4; }
+}
+
+let f = new F();
+assert(f.foo() == 4);
+
+let g:D = new F();
+assert(g.foo() == 4);
+
+let h:I = new F();
+assert(h.foo() == 4);
