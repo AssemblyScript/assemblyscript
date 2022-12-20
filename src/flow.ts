f4125ac5916e244703abf3d6d1e2c1a5ff450f2b
dcode <dcode@dcode.io>
2022-12-21 00:10:21 +0100

    what if: ??!

--- src/flow.ts
+++ src/flow.ts
@@ -729,14 +729,14 @@ export class Flow {
     let rightTargetFunction = right.targetFunction;
     if (leftTargetFunction != rightTargetFunction) {
       console.log("left: " + left.toString());
-      let iter = left.parent;
+      // let iter = left.parent;
       console.log("- ?");
       // while (iter) {
       //   console.log("- " + iter.toString());
       //   iter = iter.parent;
       // }
       console.log("right: " + right.toString());
-      iter = right.parent;
+      let iter = right.parent;
       while (iter) {
         console.log("- " + iter.toString());
         iter = iter.parent;
