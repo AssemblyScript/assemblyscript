6361ce9cf7882fb55ee41ce14ac40fe5b138067e
dcode <dcode@dcode.io>
2023-01-24 14:47:18 +0100

    update fixture

--- tests/parser/type.ts.fixture.ts
+++ tests/parser/type.ts.fixture.ts
@@ -5,11 +5,11 @@ export type T1 = int32_t;
 export type T2 = int32_t;
 export type T11 = T1 | null;
 export type T12 = T1 | null;
-// ERROR 2456: "Type alias 'T3' circularly references itself." in type.ts(11,23+4)
-// ERROR 100: "Not implemented: Recursion in type aliases" in type.ts(12,29+3)
-// ERROR 100: "Not implemented: Recursion in type aliases" in type.ts(13,24+2)
-// ERROR 100: "Not implemented: Recursion in type aliases" in type.ts(14,31+1)
-// ERROR 100: "Not implemented: Recursion in type aliases" in type.ts(15,26+1)
-// ERROR 100: "Not implemented: Recursion in type aliases" in type.ts(16,39+1)
-// ERROR 100: "Not implemented: Recursion in type aliases" in type.ts(17,32+1)
-// ERROR 100: "Not implemented: Recursion in type aliases" in type.ts(18,25+1)
+// ERROR 2456: "Type alias 'T3' circularly references itself." in type.ts(11,13+2)
+// ERROR 2456: "Type alias 'T4' circularly references itself." in type.ts(12,13+2)
+// ERROR 2456: "Type alias 'T5' circularly references itself." in type.ts(13,13+2)
+// ERROR 2456: "Type alias 'T6' circularly references itself." in type.ts(14,13+2)
+// ERROR 2456: "Type alias 'T7' circularly references itself." in type.ts(15,13+2)
+// ERROR 2456: "Type alias 'T8' circularly references itself." in type.ts(16,13+2)
+// ERROR 2456: "Type alias 'T9' circularly references itself." in type.ts(17,13+2)
+// ERROR 2456: "Type alias 'T10' circularly references itself." in type.ts(18,13+3)
