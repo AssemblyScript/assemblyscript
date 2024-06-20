ea621ce36c39225d261ac47607b4e9d87be307ee
Congcong Cai <congcongcai0907@163.com>
2024-06-21 00:19:07 +0800

    support bun

--- tests/compiler/bindings/esm.release.js
+++ tests/compiler/bindings/esm.release.js
@@ -553,8 +553,9 @@ export const {
   fn,
 } = await (async url => instantiate(
   await (async () => {
-    if (typeof self === "object" && self.constructor) { return await globalThis.WebAssembly.compileStreaming(globalThis.fetch(url)); }
-    else { return globalThis.WebAssembly.compile(await (await import("node:fs/promises")).readFile(url)); }
+    const isNodeOrBun = typeof process != "undefined" && process.versions != null && (process.versions.node != null || process.versions.bun != null);
+    if (isNodeOrBun) { return globalThis.WebAssembly.compile(await (await import("node:fs/promises")).readFile(url)); }
+    else { return await globalThis.WebAssembly.compileStreaming(globalThis.fetch(url)); }
   })(), {
   }
 ))(new URL("esm.release.wasm", import.meta.url));
