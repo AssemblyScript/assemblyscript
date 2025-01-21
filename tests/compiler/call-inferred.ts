9a7a6e03679c12be24c55f9f05d3090e9042abb3
Matt Johnson-Pint <mjp@hypermode.com>
2025-01-21 07:46:56 -0800

    feat: Constructor generic type inference (#2894)

--- tests/compiler/call-inferred.ts
+++ tests/compiler/call-inferred.ts
@@ -12,6 +12,27 @@ function bar<T>(a: T = <f32>42.0): T {
 
 assert(bar() == 42);
 
+class Foo<T> {
+  constructor(public value: T) {}
+  static create<T>(value: T): Foo<T> {
+    return new Foo(value);
+  }
+}
+
+assert(new Foo(42).value == 42);
+assert(Foo.create(42).value == 42);
+
+class Bar {
+  doSomething<T>(a: T): T {
+    return a;
+  }
+}
+
+assert(new Bar().doSomething(42) == 42);
+
+class Baz<T> extends Foo<T> {}
+assert(new Baz(42).value == 42);
+
 // TODO: this'd require return type inference, i.e., omitted return type
 // function baz<T>(a: i32): T {
 //   return a;
