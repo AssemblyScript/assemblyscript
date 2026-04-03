fae279c56a8dbdbf0f50d69adb49bc11e2115b6b
Changqing Jing <changqing.jing@bmw.com>
2026-04-03 17:25:37 +0800

    Add test

--- tests/compiler/override-typeparam-mismatch.ts
+++ tests/compiler/override-typeparam-mismatch.ts
@@ -11,4 +11,17 @@ class CC implements I {
 let c:I = new CC();
 c.foo(1);
 
+interface J {
+  bar(x: i32): i32;
+}
+
+class DD implements J {
+  bar<T>(x: i32): i32 {
+    return x;
+  }
+}
+
+let dd:DD = new DD();
+dd.bar<i32>(1);
+
 ERROR("EOF");
