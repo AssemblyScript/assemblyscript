93f7b3120365660b734f0b3e16ac336c106249df
Abdiel Lopez <48071553+PaperPrototype@users.noreply.github.com>
2026-04-15 16:31:18 -0400

    undo unecessary comment changes

--- cli/index.d.ts
+++ cli/index.d.ts
@@ -276,10 +276,7 @@ export abstract class Transform {
   /** Called when parsing is complete, before a program is instantiated from the AST. */
   afterParse?(parser: Parser): void | Promise<void>;
 
-  /**
-   * Called after the program is instantiated and before compilation-time validation runs.
-   * This is the last hook where transforms can rewrite preserved AST-only syntax before it is rejected.
-   */
+  /** Called after the program is instantiated. */
   afterInitialize?(program: Program): void | Promise<void>;
 
   /** Called when compilation is complete, before the module is being validated. */
